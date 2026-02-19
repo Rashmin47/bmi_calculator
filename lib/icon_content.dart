0487123854150255298cd464d836de81783cd31f
rashmin <rashminxperience@gmail.com>
2026-02-19 12:42:15 +0545

    Added Input Page

---
+++ lib/icon_content.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+class IconContent extends StatelessWidget {
+  const IconContent({
+    super.key, required this.text, required this.icon,
+  });
+  final String text;
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, size: 80),
+        SizedBox(height: 15),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 18,
+            color: Color(0xFF8D8E98),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,15 @@
+import 'dart:ui';
+
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'icon_content.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
+const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -8,6 +19,28 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColor = inactiveCardColour;
+  Color femaleCardColor = inactiveCardColour;
+
+  void updateColor(int gender) {
+    if (gender == 1) {
+      if(maleCardColor == inactiveCardColour){
+        maleCardColor = activeCardColour;
+        femaleCardColor = inactiveCardColour;
+    }else {
+      maleCardColor = inactiveCardColour;
+    }
+    }
+    if (gender == 2) {
+      if(femaleCardColor == inactiveCardColour){
+        femaleCardColor = activeCardColour;
+        maleCardColor = inactiveCardColour;
+      }else {
+        femaleCardColor = inactiveCardColour;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +48,80 @@ class _InputPageState extends State<InputPage> {
         backgroundColor: Color(0xFF0A0E21),
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(child: Text('Body Text')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColor(1);
+                      });
+                    },
+                    child: ReusableCard(
+                      colour: maleCardColor,
+                      cardChild: IconContent(
+                        text: 'MALE',
+                        icon: FontAwesomeIcons.mars,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColor(2);
+                      });
+                    },
+                    child: ReusableCard(
+                      colour: femaleCardColor,
+                      cardChild: IconContent(
+                        text: 'FEMALE',
+                        icon: FontAwesomeIcons.venus,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: activeCardColour,
+              cardChild: SizedBox(),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: SizedBox(),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: SizedBox(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top: 10),
 
+            width: double.infinity,
+
+            height: 80.0,
+          ),
+        ],
+      ),
     );
   }
 }
